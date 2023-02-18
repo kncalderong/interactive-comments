15cbf9c6b3725720ae861c5d98ecfb4c4dc095cb
Kevin Nicolas <kncalderong@gmail.com>
2023-02-18 10:49:43 -0500

    hover animations added

--- client/src/components/buttons/Reply.tsx
+++ client/src/components/buttons/Reply.tsx
@@ -9,7 +9,7 @@ const Reply = (props: ReplyProps) => {
   const { inMobile} = props
   
   return (
-    <div className={`flex items-center gap-2 ${inMobile ? 'sm:hidden' : 'hidden sm:flex' }`}>
+    <div className={`flex items-center gap-2 ${inMobile ? 'sm:hidden' : 'hidden sm:flex'} cursor-pointer`}>
       <img src={IconReply} alt="replyIcon" /> <span className='text-moderate-blue font-semibold' >Reply</span>
     </div>
   )
