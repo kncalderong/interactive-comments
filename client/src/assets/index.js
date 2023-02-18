c099b7b245e6e15f34ec3dd589bd58fee173aaed
Kevin Nicolas <kncalderong@gmail.com>
2023-02-18 10:49:19 -0500

    feat: conditional icon to show on hover developed

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -3,5 +3,7 @@ import IconPlus from './icon-plus.svg'
 import IconMinus from './icon-minus.svg'
 import IconDelete from './icon-delete.svg'
 import IconEdit from './icon-edit.svg'
+import IconPlusDark from './icon-plus-dark.svg'
+import IconMinusDark from './icon-minus-dark.svg'
 
-export { IconReply, IconPlus, IconMinus, IconDelete, IconEdit }
+export { IconReply, IconPlus, IconMinus, IconDelete, IconEdit, IconMinusDark, IconPlusDark  }
--- client/src/components/buttons/SelectQuantity.tsx
+++ client/src/components/buttons/SelectQuantity.tsx
@@ -1,15 +1,20 @@
 
-import { IconMinus, IconPlus } from '../../assets/index.js';
+import { IconMinus, IconPlus, IconPlusDark, IconMinusDark } from '../../assets/index.js';
+import {useState} from 'react'
 
 const SelectQuantity = () => {
+  
+  const [iconPlusToShow, setIconPlusToShow] = useState(IconPlus)
+  const [iconMinusToShow, setIconMinusToShow] = useState(IconMinus)
+  
   return (
-    <div className="flex p-2 bg-very-light-gray justify-between w-[100px] rounded-xl sm:flex-col sm:w-auto sm:h-[100px] ">
-      <div className='flex w-4 justify-center items-center'>
-        <img src={IconPlus} alt="plusIcon" />
+    <div className="flex p-2 bg-very-light-gray justify-between w-[100px] rounded-lg sm:flex-col sm:w-auto sm:h-[100px] sm:mr-6 sm:p-[0.6rem] ">
+      <div className='flex w-4 justify-center items-center basis-6 cursor-pointer ' onMouseEnter={()=> setIconPlusToShow(IconPlusDark)} onMouseLeave={()=> setIconPlusToShow(IconPlus)} >
+        <img src={iconPlusToShow} alt="plusIcon" />
       </div>
-      <div className='flex w-4 justify-center items-center text-grayish-blue font-bold '>12</div>
-      <div className='flex w-4 justify-center items-center'>
-        <img src={IconMinus} alt="minusIcon" />
+      <div className='flex w-4 justify-center items-center text-moderate-blue font-bold basis-6 '>12</div>
+      <div className='flex w-4 justify-center items-center basis-6 cursor-pointer' onMouseEnter={()=> setIconMinusToShow(IconMinusDark)} onMouseLeave={()=> setIconMinusToShow(IconMinus)}>
+        <img src={iconMinusToShow} alt="minusIcon"  />
       </div>
     </div>
   )
