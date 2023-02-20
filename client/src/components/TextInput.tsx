db58cde3689a31f23b7c521cb458b5e0223237d1
Kevin Nicolas <kncalderong@gmail.com>
2023-02-20 18:41:48 -0500

    feat: dynamic values for TextInput component added

--- client/src/components/TextInput.tsx
+++ client/src/components/TextInput.tsx
@@ -1,10 +1,11 @@
 
+import currentUser from "../utils/CurrentUser"
+
 type TextInputProps = {
   isEditing: boolean
   isReplying: boolean
   initialText?: string
 }
-
 
 const TextInput = (props: TextInputProps) => {
 
@@ -18,7 +19,7 @@ const TextInput = (props: TextInputProps) => {
     <form onSubmit={onSubmit} className={`${isEditing ? 'w-[100%] p-0 sm:p-0' : 'w-[95%] p-4 sm:p-6'} bg-white rounded-lg sm:max-w-[720px]  flex flex-col sm:flex-row sm:items-start sm:gap-5 ${isEditing && 'sm:flex-col sm:items-end sm:gap-0'}  `}>
       {!isEditing && (
         <div className="w-[2rem] hidden sm:block ">
-          <img src="https://user-images.githubusercontent.com/80135017/218482952-66003814-ad3e-4482-b76e-694cf41368a5.png" alt="userImage" className="w-full block" />
+          <img src={currentUser.image } alt="userImage" className="w-full block" />
         </div>)}
       <div className={`mb-2 sm:grow ${isEditing && 'w-[100%]'}`} >
         <textarea name="contentText" id="contentText" cols={30} rows={isEditing ? 4 : 3} placeholder="Add a comment..." className="resize-none focus-visible:outline-none border-light-gray rounded-md border-2 w-[100%] px-5 py-2 placeholder-grayish-blue text-grayish-blue  " defaultValue={(initialText !== undefined) ? initialText : ''}  ></textarea>
@@ -26,7 +27,7 @@ const TextInput = (props: TextInputProps) => {
       <div className={ `flex  items-center ${isEditing ? 'mb-2 justify-end' : 'justify-between'}`} >
         {!isEditing && (
           <div className="w-[2rem] block sm:hidden ">
-            <img src="https://user-images.githubusercontent.com/80135017/218482952-66003814-ad3e-4482-b76e-694cf41368a5.png" alt="userImage" className="w-full block" />
+            <img src={currentUser.image} alt="userImage" className="w-full block" />
           </div>
         )}
         <button type="submit" className="bg-moderate-blue text-white px-6 py-2 text-[0.9rem] rounded-md uppercase " >
