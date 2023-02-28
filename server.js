033d819896c2b2ae2b618131ec6e3f8ba18e657a
Kevin Nicolas <kncalderong@gmail.com>
2023-02-28 16:35:54 -0500

    feat: deployment stage - correct path for server side rendering added

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 
-import Comment from './models/Comment.js'
-import User from  './models/User.js'
-
 //to handle errors throught middleware
 import 'express-async-errors'
+
+//to redirect front build to our server side rendering correct path
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 // db and authenticateUser
 import connectDB from './db/connect.js';
@@ -19,12 +21,18 @@ import commentsRouter from './routes/commentsRoutes.js'
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './client/build'))); //this middlewere allows access to all static files
+
 app.use(express.json());
 app.use('/api/v1/comments', commentsRouter);
 
-app.get('/api/v1', (req, res) => {
-  res.json({msg: 'hello world sdfasdf'})
-})
+// only when ready to deploy
+app.get('*', (req, res) => { //this middleware allows me to handle every possible route in the front end through the index.html, but it always must be called after my server routes (the above ones)
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
