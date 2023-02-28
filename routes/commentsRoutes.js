3ab39dc3dc067756599f796a684a3d9fca48dd75
Kevin Nicolas <kncalderong@gmail.com>
2023-02-28 17:46:55 -0500

    feat: api rate limit set to prevent too many creation of comments from one IP

--- routes/commentsRoutes.js
+++ routes/commentsRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express';
 const router = express.Router()
+import rateLimiter from 'express-rate-limit'
 
-import {createComment, updateComment,deleteComment, getAllComments } from '../controllers/commentsController.js'
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // 10 request per 15 minutes
+  messageLimit: 'to many requests from this Ip address, please try again after 15 minutes'
+})
+  
+import { createComment, updateComment, deleteComment, getAllComments } from '../controllers/commentsController.js'
 
-router.route('/').post(createComment).get(getAllComments)
+router.route('/').post(apiLimiter, createComment).get(getAllComments) // I will limite the number of comments created by One IP direction in  15minutes window
 router.route('/:id').patch(updateComment).delete(deleteComment)
 export default router
